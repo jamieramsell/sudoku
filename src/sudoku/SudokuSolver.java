fa8f528e463adc67eb71859e4451846e6c7299df
Jamie <jamieramsell07@outlook.com>
2026-05-22 05:08:33 +0100

    refactor: move isValidMove method to a new position for better readability

--- src/sudoku/SudokuSolver.java
+++ src/sudoku/SudokuSolver.java
@@ -50,26 +50,6 @@ public class SudokuSolver implements ISudokuSolver {
     return solutions;
   }
 
-  @Override
-  public boolean isValidMove(int row, int column, int value) {
-    
-    // Input Validation //
-
-    if (value == -1) { // Always allow cells to be emptied
-      return true;
-    } else if (value < 1 || value > 9) {
-      throw new IllegalArgumentException("value must be either -1, or between 1 and 9 inclusive.");
-    }
-
-    int[] grid_size = grid.getGridSize();
-    if (row < 0 || row >= grid_size[0] * 3 || column < 0 || column >= grid_size[1] * 3) {
-      throw new IndexOutOfBoundsException("row or column out of bounds");
-    }
-
-    return ISudokuSolver.isPlacementValid(grid.getGrid(), row, column, value);
-
-  }
-  
   // Convenience method to contain the exhaustive search which finds all solutions to a puzzle in
   // a given state.
   private static void solve(int[][] workingGrid, Set<int[][]> solutions, int solutions_required) {
@@ -110,6 +90,26 @@ public class SudokuSolver implements ISudokuSolver {
       }
     }
     return null;
+  }
+
+  @Override
+  public boolean isValidMove(int row, int column, int value) {
+    
+    // Input Validation //
+
+    if (value == -1) { // Always allow cells to be emptied
+      return true;
+    } else if (value < 1 || value > 9) {
+      throw new IllegalArgumentException("value must be either -1, or between 1 and 9 inclusive.");
+    }
+
+    int[] grid_size = grid.getGridSize();
+    if (row < 0 || row >= grid_size[0] * 3 || column < 0 || column >= grid_size[1] * 3) {
+      throw new IndexOutOfBoundsException("row or column out of bounds");
+    }
+
+    return ISudokuSolver.isPlacementValid(grid.getGrid(), row, column, value);
+
   }
 
 }
