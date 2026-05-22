0c94cb4cdab5f0169015c06661e74ce3668c4b78
Jamie <jamieramsell07@outlook.com>
2026-05-22 04:52:00 +0100

    feat: implement full grid generation in SudokuGenerator with random but legal cell values

--- src/sudoku/SudokuGenerator.java
+++ src/sudoku/SudokuGenerator.java
@@ -1,9 +1,13 @@
 package sudoku;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class SudokuGenerator implements ISudokuGenerator{
 
   private final PuzzleDifficulty difficulty;
   private ISudokuGrid grid;
+  private final Tuple2<Integer, Integer> grid_size;
 
   /**
    * Initialises a {@code SudokuGenerator} with the given target difficulty.
@@ -13,7 +17,10 @@ public class SudokuGenerator implements ISudokuGenerator{
    */
   public SudokuGenerator(PuzzleDifficulty difficulty) {
     this.difficulty = difficulty;
-    grid = new SudokuGrid();
+    this.grid = new SudokuGrid();
+
+    int[] grid_dimensions = grid.getGridSize();
+    this.grid_size = new Tuple2<>(grid_dimensions[0] * 3, grid_dimensions[1] * 3);
   }
 
   @Override
@@ -32,9 +39,30 @@ public class SudokuGenerator implements ISudokuGenerator{
   /**
    * Convenience method to generate a random completely filled Sudoku puzzle.
    * <p>This method acts directly upon {@code grid}, rather than returning an updated version.
+   * @param hasUniqueSolution Whether or not the generated puzzle should only have one solution.
    */
   private void generateFullGrid() {
-    throw new UnsupportedOperationException();
+
+    // Firstly generate a random cell
+    int random_value = randomInteger(1, 10);
+    int random_x = randomInteger(0, grid_size.first());
+    int random_y = randomInteger(0, grid_size.second());
+    grid.setValue(random_x, random_y, random_value);
+
+    // Then compute a random solution
+    ISudokuSolver solver = new SudokuSolver(grid);
+    List<int[][]> solutions = new ArrayList<>();
+    solutions.addAll(solver.solveGrid());
+    int num_solutions = solutions.size();
+    int[][] solution = solutions.get(randomInteger(0, num_solutions));
+
+    // Finally, update grid attribute with solved values of cells
+    for (int row = 0; row < grid_size.first(); row++) {
+      for (int col = 0; col < grid_size.second(); col++) {
+        grid.setValue(row, col, solution[row][col]);
+      }
+    }
+    
   }
 
   /**
@@ -46,6 +74,17 @@ public class SudokuGenerator implements ISudokuGenerator{
    */
   private boolean removeCells() {
     throw new UnsupportedOperationException();
+  }
+
+  /**
+   * Convenience function to generate a random integer between the two values provided.
+   * @param lower_bound The inclusive lower bound of the number to generate.
+   * @param upper_bound The upper bound of the number to generate. (non-inclusive)
+   * @return A random number {@code lower_bound <= n < upper_bound}
+   */
+  static private int randomInteger(int lower_bound, int upper_bound) {
+    int num_possibilities = upper_bound - lower_bound;
+    return lower_bound + ((int) Math.random()) * (num_possibilities);
   }
 
 
