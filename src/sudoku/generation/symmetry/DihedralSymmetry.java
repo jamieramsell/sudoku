1f7e00654cfec0bd6940ea43840bd6e7a67b15f6
Jamie <jamieramsell07@outlook.com>
2026-06-07 00:46:49 +0100

    fix: update DihedralSymmetry.getSymmetricCells() to use a set for duplicate elimination

--- src/sudoku/generation/symmetry/DihedralSymmetry.java
+++ src/sudoku/generation/symmetry/DihedralSymmetry.java
@@ -1,7 +1,9 @@
 package sudoku.generation.symmetry;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 
 import sudoku.Tuple2;
 
@@ -23,9 +25,12 @@ public class DihedralSymmetry extends AbstractSymmetry {
   public List<Tuple2<Integer, Integer>> getSymmetricCells(int row, int col) {
     checkBounds(row, col);
 
-    List<Tuple2<Integer, Integer>> cells = new ArrayList<>();
-    cells.addAll(rotational.getSymmetricCells(row, col));
-    cells.addAll(reflectional.getSymmetricCells(row, col));
+    // Use a set to avoid any duplicates
+    Set<Tuple2<Integer, Integer>> cell_set = new HashSet<>();
+    cell_set.addAll(rotational.getSymmetricCells(row, col));
+    cell_set.addAll(reflectional.getSymmetricCells(row, col));
+
+    List<Tuple2<Integer, Integer>> cells = new ArrayList<>(cell_set);
 
     return cells;
   }
