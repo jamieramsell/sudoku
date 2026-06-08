56b835b503e01ef7f3259055cbd00ebadc774618
Jamie <jamieramsell07@outlook.com>
2026-06-08 21:36:35 +0100

    docs: fix inconsistency between rows and columns, and x and y

--- src/sudoku/ISudokuGrid.java
+++ src/sudoku/ISudokuGrid.java
@@ -6,8 +6,8 @@ public interface ISudokuGrid {
    * Gets the current value of a cell in a sudoku grid.
    * <p>Coordinates are indexed from (0, 0), which is the upper left-most cell in the grid.
    * @author Jamie
-   * @param row The row, or x-coordinate, of the cell
-   * @param column The column, or y-coordinate, of the cell
+   * @param row The row, or y-coordinate, of the cell
+   * @param column The column, or x-coordinate, of the cell
    * @return The value currently stored at the cell, or -1 if the cell is empty.
    */
   public int getValue(int row, int column);
@@ -17,8 +17,8 @@ public interface ISudokuGrid {
    * <p>Coordinates are indexed from (0, 0), which is the upper left-most cell in the grid.
    * <p>Note that {@code 1 <= value <= 9}, or to set the cell as empty, {@code value = -1}
    * @author Jamie
-   * @param row The row, or x-coordinate, of the cell
-   * @param column The column, or y-coordinate, of the cell
+   * @param row The row, or y-coordinate, of the cell
+   * @param column The column, or x-coordinate, of the cell
    * @param value The value of the cell
    */
   public void setValue(int row, int column, int value);
